262064ec266c72800d63e53c688da2e9308929b8
By2Hash <by2git2@gmail.com>
2025-09-12 12:34:54 -0300

    Terminado el frmBotonera

--- pryPereiroGUI/frmBotonera.Designer.cs
+++ pryPereiroGUI/frmBotonera.Designer.cs
@@ -30,42 +30,63 @@
         {
             button1 = new Button();
             button2 = new Button();
-            label1 = new Label();
+            lblNombre = new Label();
+            btnAgregar = new Button();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Image = Properties.Resources.arrow_arrows_back_direction_left_navigation_right_icon_123237;
-            button1.Location = new Point(133, 177);
+            button1.Location = new Point(180, 182);
             button1.Name = "button1";
-            button1.Size = new Size(50, 50);
+            button1.Size = new Size(49, 32);
             button1.TabIndex = 0;
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
             button2.Image = Properties.Resources.arrow_arrows_back_direction_left_navigation_right_icon_123236__1_;
-            button2.Location = new Point(205, 177);
+            button2.Location = new Point(249, 182);
             button2.Name = "button2";
-            button2.Size = new Size(50, 50);
+            button2.Size = new Size(49, 32);
             button2.TabIndex = 1;
             button2.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblNombre
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(167, 127);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 2;
-            label1.Text = "label1";
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Segoe UI", 9.7F);
+            lblNombre.Location = new Point(180, 104);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(118, 17);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Ingrese su nombre";
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(180, 153);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(118, 23);
+            btnAgregar.TabIndex = 3;
+            btnAgregar.Text = "button3";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(180, 124);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(118, 23);
+            textBox1.TabIndex = 4;
             // 
             // frmBotonera
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(484, 461);
-            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(btnAgregar);
+            Controls.Add(lblNombre);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "frmBotonera";
@@ -79,6 +100,8 @@
 
         private Button button1;
         private Button button2;
-        private Label label1;
+        private Label lblNombre;
+        private Button btnAgregar;
+        private TextBox textBox1;
     }
 }
